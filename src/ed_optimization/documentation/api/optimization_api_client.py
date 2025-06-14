11f537c31a324db5a9243f3db509b141204aa203
ffekirnew <ffekirnew0808@gmail.com>
2025-06-14 11:10:20 +0300

    fix: Minor fix

--- src/ed_optimization/documentation/api/optimization_api_client.py
+++ src/ed_optimization/documentation/api/optimization_api_client.py
@@ -1,8 +1,8 @@
 from ed_domain.documentation.api.definitions import ApiResponse
+from ed_infrastructure.documentation.api.endpoint_client import EndpointClient
 
 from ed_optimization.application.features.order.dtos import (
     CalculateOrderDetailsDto, CreateOrderDto, RouteInformationDto)
-from ed_optimization.common.api_helpers import ApiClient
 from ed_optimization.documentation.api.abc_optimization_api_client import \
     ABCOptimizationApiClient
 from ed_optimization.documentation.api.optimization_endpoint_descriptions import \
@@ -15,7 +15,7 @@ class OptimizationApiClient(ABCOptimizationApiClient):
 
     async def create_order(self, create_order_dto: CreateOrderDto) -> ApiResponse[None]:
         endpoint = self._endpoints.get_description("create_order")
-        api_client = ApiClient[None](endpoint)
+        api_client = EndpointClient[None](endpoint)
 
         return await api_client({"request": create_order_dto})
 
@@ -23,6 +23,6 @@ class OptimizationApiClient(ABCOptimizationApiClient):
         self, calculate_order_details_dto: CalculateOrderDetailsDto
     ) -> ApiResponse[RouteInformationDto]:
         endpoint = self._endpoints.get_description("calculate_order_details")
-        api_client = ApiClient[RouteInformationDto](endpoint)
+        api_client = EndpointClient[RouteInformationDto](endpoint)
 
         return await api_client({"request": calculate_order_details_dto})
